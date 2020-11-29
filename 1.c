d4d6af5ee18363d58c10ae30cbdb201301f44dd3
Rachel0305 <2465357501@qq.com>
2020-11-29 13:53:50 +0800

    隔天再来试一试

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i，j;
+
+    for (i = 0; i < 3; ++i)
+        printf("heihei\n");
+        for (j = 2; j < 5; ++j)
+            printf("haha\n");
+    printf("xixi\n");
+
+    return 0;
+}
